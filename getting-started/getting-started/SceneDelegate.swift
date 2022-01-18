02159af79ef0dff2596884fba91f2213caab226b
Karolina Zydek <karolina.zydek@pubnub.com>
2022-01-18 20:31:38 +0100

    Unify uuid, channel, and key values

--- getting-started/getting-started/SceneDelegate.swift
+++ getting-started/getting-started/SceneDelegate.swift
@@ -11,24 +11,24 @@ import PubNub
 import PubNubChat
 import PubNubChatComponents
 
-let PUBNUB_PUBLISH_KEY = "demo" // "pub-c-key"
-let PUBNUB_SUBSCRIBE_KEY = "demo" // "sub-c-key"
+let PUBNUB_PUBLISH_KEY = "myPublishKey"
+let PUBNUB_SUBSCRIBE_KEY = "myPublishKey"
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
-  // Create PubNub Configuration
+  // Creates PubNub configuration
   lazy var pubnubConfiguration = {
     return PubNubConfiguration(
       publishKey: PUBNUB_PUBLISH_KEY,
       subscribeKey: PUBNUB_SUBSCRIBE_KEY,
-      uuid: "uuid-of-current-user"
+      uuid: "myFirstUser"
     )
   }()
   
   var window: UIWindow?
 
   var chatProvider: PubNubChatProvider?
-  var defaultChannelId = "my-current-channel"
+  var defaultChannelId = "Default"
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -38,53 +38,53 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     let window = UIWindow(windowScene: windowScene)
     
-    // Enable PubNub logging to the Console
+    // Enables PubNub logging to the Console
     PubNub.log.levels = [.all]
     PubNub.log.writers = [ConsoleLogWriter()]
     
     if chatProvider == nil {
-      // Create a new ChatProvider
+      // Creates a new ChatProvider
       let provider = PubNubChatProvider(
         pubnubConfiguration: pubnubConfiguration
       )
       
-      // Preload Dummy Data
+      // Preloads dummy data
       preloadData(provider)
       
-      // Assign to SceneDelegate for future use
+      // Assigns to SceneDelegate for future use
       chatProvider = provider
     }
     
-    // Create the default Channel List and Member List Component View Models
+    // Creates the default ChannelList and MemberList component view models
     guard let channelListViewModel = chatProvider?.senderMembershipsChanneListComponentViewModel(),
           let messageListViewModel = try? chatProvider?.messageListComponentViewModel(pubnubChannelId: defaultChannelId) else {
       preconditionFailure("Could not create intial view models")
     }
     
-    // Create navigation structure
+    // Creates the navigation structure
     let navigation = UINavigationController()
   
-    // Load the MessageList as the root view, but allow for the ChannelList to be the `Back` view
+    // Loads the MessageList as the root view, but allows for the ChannelList to be the previous view
     navigation.viewControllers = [
       channelListViewModel.configuredComponentView(),
       messageListViewModel.configuredComponentView()
     ]
 
-    // Set the component as the root view controller
+    // Sets the component as the root view controller
     window.rootViewController = navigation
     self.window = window
     window.makeKeyAndVisible()
   }
   
   func preloadData(_ chatProvider: PubNubChatProvider) {
-    // Create a user object with UUID
+    // Creates a user object with uuid
     let user = PubNubChatUser(
       id: chatProvider.pubnubConfig.uuid,
-      name: "You",
+      name: "myFirstUser",
       avatarURL: URL(string: "https://picsum.photos/seed/\(chatProvider.pubnubConfig.uuid)/200")
     )
     
-    // Create a channel object
+    // Creates a channel object
     let channel = PubNubChatChannel(
       id: defaultChannelId,
       name: "Default",
@@ -92,13 +92,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       avatarURL: URL(string: "https://picsum.photos/seed/\(defaultChannelId)/200")
     )
     
-    // Create a membership between the User and the Channel for subscription purposes
+    // Creates a membership between the user and the channel for subscription purposes
     let membership = PubNubChatMember(channel: channel, member: user)
     
-    // Subscribe to the default channel
+    // Subscribes to the default channel
     chatProvider.pubnubProvider.subscribe(.init(channels: [defaultChannelId], withPresence: true))
     
-    // Fill database with the user, channel, and memberships data
+    // Fills the database with the user, channel, and memberships data
     chatProvider.dataProvider.load(members: [membership])
   }
 }
